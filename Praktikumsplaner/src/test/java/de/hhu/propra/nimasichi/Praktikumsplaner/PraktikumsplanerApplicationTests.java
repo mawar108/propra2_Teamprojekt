8ba04d5d6c1fc7e67667f45b6d70def4f00ad5e5
Couraxe <xvertexsun@gmail.com>
2021-03-01 10:04:44 +0100

    Add Sprin intialized project

---
+++ Praktikumsplaner/src/test/java/de/hhu/propra/nimasichi/Praktikumsplaner/PraktikumsplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.nimasichi.Praktikumsplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumsplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
