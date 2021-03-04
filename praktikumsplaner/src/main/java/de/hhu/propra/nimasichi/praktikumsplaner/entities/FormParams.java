be60d37f434b4abbf62488bb5fffb6f3ce0984ae
Wartisch <mawar108@uni-duessldorf.de>
2021-03-04 09:43:48 +0100

    PMD-Fehler größtenteils behoben

---
+++ praktikumsplaner/src/main/java/de/hhu/propra/nimasichi/praktikumsplaner/entities/FormParams.java
@@ -0,0 +1,17 @@
+package de.hhu.propra.nimasichi.praktikumsplaner.entities;
+
+import lombok.Value;
+
+@Value
+@SuppressWarnings({"PMD.DefaultPackage", "PMD.CommentDefaultAccessModifier"})
+public class FormParams {
+
+	String name;
+	int modus;
+	String anStartdatum;
+	String anStartzeit;
+	String anSchlussdatum;
+	String anSchlusszeit;
+	int minPersonen;
+	int maxPersonen;
+}
