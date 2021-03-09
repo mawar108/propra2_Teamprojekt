299bd43fc599591588bef4660b2d78b823f8b4e4
Wartisch <mawar108@uni-duessldorf.de>
2021-03-09 09:09:42 +0100

    fix webconfig

--- praktikumsplaner/src/main/java/de/hhu/propra/nimasichi/praktikumsplaner/config/WebSecurityConfiguration.java
+++ praktikumsplaner/src/main/java/de/hhu/propra/nimasichi/praktikumsplaner/config/WebSecurityConfiguration.java
@@ -1,24 +1,18 @@
 package de.hhu.propra.nimasichi.praktikumsplaner.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.LawOfDemeter"})
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(final HttpSecurity http) throws Exception {
-    http.authorizeRequests(a -> a
-        .antMatchers("/**").permitAll())
-        .exceptionHandling(e -> e.authenticationEntryPoint(
-            new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-        .csrf(c -> c.csrfTokenRepository(
-            CookieCsrfTokenRepository.withHttpOnlyFalse()))
-        .logout(l -> l.logoutSuccessUrl("/").permitAll())
-        .oauth2Login();
+    http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2Login();
   }
 }
