82da2a87c7f9852c642358331eb7a71e683ed7cb
Couraxe <xvertexsun@gmail.com>
2021-03-14 17:45:17 +0100

    Squashed commit of the following:
    commit 46698014bc053e078baf76841b22b887fd8534a7
    Author: Couraxe <xvertexsun@gmail.com>
    Date:   Sun Mar 14 17:45:07 2021 +0100
    
        Cleanup

--- praktikumsplaner/src/test/java/de/hhu/propra/nimasichi/praktikumsplaner/unit/LetzteTutorTerminServiceTests.java
+++ praktikumsplaner/src/test/java/de/hhu/propra/nimasichi/praktikumsplaner/unit/LetzteTutorTerminServiceTests.java
@@ -1,7 +1,7 @@
 package de.hhu.propra.nimasichi.praktikumsplaner.unit;
 
-import de.hhu.propra.nimasichi.praktikumsplaner.domain.ubungswocheconfig.UbungswocheConfig;
 import de.hhu.propra.nimasichi.praktikumsplaner.domain.ubungswocheconfig.TutorTermin;
+import de.hhu.propra.nimasichi.praktikumsplaner.domain.ubungswocheconfig.UbungswocheConfig;
 import de.hhu.propra.nimasichi.praktikumsplaner.repositories.UbungswocheConfigRepo;
 import de.hhu.propra.nimasichi.praktikumsplaner.services.ubungsconfig.LetzteTutorTermineService;
 import org.junit.jupiter.api.DisplayName;
@@ -22,12 +22,12 @@ public class LetzteTutorTerminServiceTests {
   @Test
   @DisplayName("Das Repo ist leer, es werden keine Tutor Termine zuruckgegeben")
   public void testLeeresRepository() {
-    var repo = mock(UbungswocheConfigRepo.class);
+    final var repo = mock(UbungswocheConfigRepo.class);
+    final var ttService = new LetzteTutorTermineService(repo);
+
     when(repo.findByHighestId()).thenReturn(Optional.empty());
 
-    LetzteTutorTermineService ttService = new LetzteTutorTermineService(repo);
-
-    Set<TutorTermin> neueTutorTermine
+    final var neueTutorTermine
         = ttService.getNeueTutorTermine("2020-01-01", "00:00");
 
     assertThat(neueTutorTermine).isEmpty();
@@ -37,30 +37,28 @@ public class LetzteTutorTerminServiceTests {
   @DisplayName("Im Repo wird ein Tutor Termin gefunden,"
       + "das Datum wird um eine Wochen verschoben")
   public void testEineWocheVerschieben() {
-    var repo = mock(UbungswocheConfigRepo.class);
-
-    var alteTermine = Set.of(
-        new TutorTermin("Max", LocalDateTime.of(2020, 1, 1, 10, 30))
+    final var anmeldeSchlussdatum = "2020-01-03";
+    final var anmeldeSchlusszeit =  "00:00";
+    final var repo = mock(UbungswocheConfigRepo.class);
+    final var alteTermine = Set.of(
+        new TutorTermin("Max", LocalDateTime.of(2020, 1, 1, 10,
+            30))
     );
+    final var config = mock(UbungswocheConfig.class);
 
-    var config = mock(UbungswocheConfig.class);
     when(config.getTutorTermine()).thenReturn(alteTermine);
     when(repo.findByHighestId()).thenReturn(Optional.of(config));
 
-    String anmeldeSchlussdatum = "2020-01-03";
-    String anmeldeSchlusszeit =  "00:00";
-
-    LetzteTutorTermineService ttService = new LetzteTutorTermineService(repo);
-
-    Set<TutorTermin> neueTermine
+    final var ttService = new LetzteTutorTermineService(repo);
+    final var neueTermine
         = ttService.getNeueTutorTermine(anmeldeSchlussdatum,
         anmeldeSchlusszeit);
-
-    var expectedTermin
+    final var expectedTermin
         = new TutorTermin(
-            "Max",
+        "Max",
         LocalDateTime.of(
             2020, 1, 8, 10, 30));
+
     assertThat(neueTermine).contains(expectedTermin);
   }
 
@@ -69,9 +67,10 @@ public class LetzteTutorTerminServiceTests {
       + "Woche ist vor Anmeldeschluss der neuen Woche, "
       + "trotzdem werden alle Termine um eine Woche verschoben")
   public void testEineWocheZweiTermine() {
-    var repo = mock(UbungswocheConfigRepo.class);
-
-    var alteTermine = Set.of(
+    final var anmeldeSchlussdatum = "2020-01-02";
+    final var anmeldeSchlusszeit =  "00:00";
+    final var repo = mock(UbungswocheConfigRepo.class);
+    final var alteTermine = Set.of(
         new TutorTermin("Max",
             LocalDateTime.of(2020, 1, 1, 10,
                 30)),
@@ -79,22 +78,16 @@ public class LetzteTutorTerminServiceTests {
             LocalDateTime.of(2020, 1, 3, 10,
                 30))
     );
+    final var config = mock(UbungswocheConfig.class);
 
-    var config = mock(UbungswocheConfig.class);
     when(config.getTutorTermine()).thenReturn(alteTermine);
-
     when(repo.findByHighestId()).thenReturn(Optional.of(config));
 
-    String anmeldeSchlussdatum = "2020-01-02";
-    String anmeldeSchlusszeit =  "00:00";
-
-    LetzteTutorTermineService ttService = new LetzteTutorTermineService(repo);
-
-    Set<TutorTermin> neueTermine
+    final var ttService = new LetzteTutorTermineService(repo);
+    final var neueTermine
         = ttService.getNeueTutorTermine(
-            anmeldeSchlussdatum, anmeldeSchlusszeit);
-
-    var expectedTermine = Set.of(
+        anmeldeSchlussdatum, anmeldeSchlusszeit);
+    final var expectedTermine = Set.of(
         new TutorTermin("Max",
             LocalDateTime.of(2020, 1, 8, 10,
                 30)),
@@ -102,17 +95,17 @@ public class LetzteTutorTerminServiceTests {
             LocalDateTime.of(2020, 1, 10, 10,
                 30))
     );
+
     assertThat(neueTermine).containsAll(expectedTermine);
   }
 
   @Test
   @DisplayName("Wegen Ferien fallt eine Ubungswoche aus, "
       + "alle Termine werden um 2 Wochen verschoben.")
-  // Im vergleich zu letzten Test hat sich nur der Anmeldeschluss geandert!
+  // Im vergleich zum letzten Test hat sich nur der Anmeldeschluss geändert!
   public void testZweiWochenZweiTermine() {
-    var repo = mock(UbungswocheConfigRepo.class);
-
-    var alteTermine = Set.of(
+    final var repo = mock(UbungswocheConfigRepo.class);
+    final var alteTermine = Set.of(
         new TutorTermin("Max",
             LocalDateTime.of(2020, 1, 1, 10,
                 30)),
@@ -120,26 +113,24 @@ public class LetzteTutorTerminServiceTests {
             LocalDateTime.of(2020, 1, 3, 10,
                 30))
     );
-
-    var config
+    final var config
         = mock(UbungswocheConfig.class);
-    when(config.getTutorTermine()).thenReturn(alteTermine);
 
+    when(config.getTutorTermine()).thenReturn(alteTermine);
     when(repo.findByHighestId()).thenReturn(Optional.of(config));
 
-    String anmeldeSchlussdatum = "2020-01-09";
-    String anmeldeSchlusszeit =  "00:00";
-    LetzteTutorTermineService ttService = new LetzteTutorTermineService(repo);
-
-    Set<TutorTermin> neueTermine
+    final var anmeldeSchlussdatum = "2020-01-09";
+    final var anmeldeSchlusszeit =  "00:00";
+    final var ttService = new LetzteTutorTermineService(repo);
+    final var neueTermine
         = ttService.getNeueTutorTermine(anmeldeSchlussdatum, anmeldeSchlusszeit);
-
-    var expectedTermine = Set.of(
+    final var expectedTermine = Set.of(
         new TutorTermin("Max",
             LocalDateTime.of(2020, 1, 15, 10, 30)),
         new TutorTermin("Peter",
             LocalDateTime.of(2020, 1, 17, 10, 30))
     );
+
     assertThat(neueTermine).containsAll(expectedTermine);
   }
 }
